33e1139a339c9caf973f6f5f8c7eadeaf5236b4a
Melissa F <melissa.faulner@gmail.com>
2020-11-12 19:57:29 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -15,7 +15,7 @@ def literal_aoh
   # held in the constants
   aoh = [
     {name: "Don Gately", occupation: "Live-in Staff"},
-    {name: "Joelle van Dyne", occupation: "Radio"},
+    {name: "Joelle van Dyne", occupation: "Radio Personality"},
     {name: "Kate Gompert", occupation: "None"}, 
     {name: "Bruce Green", occupation: "Fan of Mildred"}
     ]
