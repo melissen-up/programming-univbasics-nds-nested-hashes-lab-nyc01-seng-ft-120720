4c1c5fbc214cb27afe12272adfa6ff53bbcd95ca
Melissa F <melissa.faulner@gmail.com>
2020-11-12 20:07:50 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -25,6 +25,6 @@ def aoh_lookup(aoh, row, key)
 end
 
 def aoh_update(aoh, row, key, new_value)
-  aoh[1][:name] = "Joelle van Dyne (PGOAT)"
-  aoh[1][:name]
+  aoh[row][key] = new_value
+  aoh
 end
